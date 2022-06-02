571251b8dddb60221ed81d8d37ffd00c6ee794e1
TechBread <olyg4000@gmail.com>
2022-06-02 22:32:51 +0500

    first back

---
+++ back/src/test/java/com/aircontrol/beta/AircontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aircontrol.beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
